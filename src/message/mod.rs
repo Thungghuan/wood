fd3f33d1b7de9d08a59188bad70c95fe206275f8
Thungghuan <1092887426@qq.com>
2022-03-02 21:11:04 +0800

    refactor: move message_chain into individual file

--- src/message/mod.rs
+++ src/message/mod.rs
@@ -1,20 +1,12 @@
 #![allow(dead_code)]
-mod sender;
-
 use serde::{Deserialize, Serialize};
 
+mod sender;
 pub use sender::Sender;
 use sender::{FriendSender, GroupSender};
 
-#[derive(Serialize, Deserialize, Debug, PartialEq)]
-#[serde(tag = "type")]
-pub enum SingleMessage {
-    Source { id: i32, time: i32 },
-    At { target: String, display: String },
-    Plain { text: String },
-}
-
-pub type MessageChain = Vec<SingleMessage>;
+mod message_chain;
+pub use message_chain::{MessageChain, SingleMessage};
 
 #[derive(Serialize, Deserialize, Debug, PartialEq)]
 #[serde(tag = "type")]
@@ -30,47 +22,6 @@ pub enum ReceivedMessage {
         sender: GroupSender,
         message_chain: MessageChain,
     },
-}
-
-#[test]
-fn check_message_chain_serialize_result() {
-    let source_message = SingleMessage::Source {
-        id: 20211113,
-        time: 20211113,
-    };
-
-    assert_eq!(
-        serde_json::to_string(&source_message).unwrap(),
-        "{\"type\":\"Source\",\"id\":20211113,\"time\":20211113}"
-    );
-
-    let at_message = SingleMessage::At {
-        target: "20211113".to_string(),
-        display: "土土木木".to_string(),
-    };
-
-    assert_eq!(
-        serde_json::to_string(&at_message).unwrap(),
-        "{\"type\":\"At\",\"target\":\"20211113\",\"display\":\"土土木木\"}"
-    );
-
-    let plain_message = SingleMessage::Plain {
-        text: "test".to_string(),
-    };
-
-    assert_eq!(
-        serde_json::to_string(&plain_message).unwrap(),
-        "{\"type\":\"Plain\",\"text\":\"test\"}"
-    );
-
-    let mut message_chain: MessageChain = vec![];
-    message_chain.push(source_message);
-    message_chain.push(plain_message);
-
-    assert_eq!(
-        serde_json::to_string(&message_chain).unwrap(),
-        "[{\"type\":\"Source\",\"id\":20211113,\"time\":20211113},{\"type\":\"Plain\",\"text\":\"test\"}]"
-    )
 }
 
 #[test]
