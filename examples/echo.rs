c5298cc5b240c007a540994ad9d4a10a03ddf260
Thungghuan <1092887426@qq.com>
2022-03-13 19:01:39 +0800

    feat: update the echo bot

--- examples/echo.rs
+++ examples/echo.rs
@@ -1,4 +1,4 @@
-use wood::message::{ChatroomType, MessageChain, SingleMessage};
+use wood::message::{create_plain_message_chain, ChatroomType};
 use wood::Bot;
 
 #[tokio::main]
@@ -6,17 +6,8 @@ async fn main() {
     let (config, session, base_url) = wood::init("config/config.yml").await;
     let mut bot = Bot::new(config, &session, &base_url);
 
-    bot.on("friendMessage", &|ctx| async move {
+    bot.command("echo", &|ctx| async move {
         ctx.reply(ctx.message_chain()).await?;
-
-        Ok(())
-    });
-
-    bot.on("groupMessage", &|ctx| async move {
-        if ctx.is_at_me() {
-            // You can reply with a quote
-            ctx.quote_reply(ctx.message_chain()).await?
-        }
 
         Ok(())
     });
@@ -27,12 +18,14 @@ async fn main() {
 
         // Send a start message to the master.
         let start_message = "Hello master, I'm an echo bot!";
-        let mut message_chain: MessageChain = vec![];
-        message_chain.push(SingleMessage::Plain {
-            text: start_message.to_string(),
-        });
-        bot.send_message(ChatroomType::Friend, bot.master_qq(), message_chain, None)
-            .await?;
+
+        bot.send_message(
+            ChatroomType::Friend,
+            bot.master_qq(),
+            create_plain_message_chain(start_message.to_string()),
+            None,
+        )
+        .await?;
 
         Ok(())
     })
